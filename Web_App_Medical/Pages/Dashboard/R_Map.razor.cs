dd08246521c88419d61247746a46d9815449d7da
longduy0901373240 <longduy0901373240@gmail.com>
2021-06-13 17:50:11 +0700

    Upload 3 Luan Van insert map

---
+++ Web_App_Medical/Pages/Dashboard/R_Map.razor.cs
@@ -0,0 +1,36 @@
+﻿using Entity.DM_AD;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_App_Medical.Pages.Dashboard
+{
+    public partial class R_Map
+    {
+        private IList<CAD_Dia_Chi> p_arrDia_Chi;
+        private CAD_Dia_Chi p_objItem = new CAD_Dia_Chi();
+        public string p_strAddress_User { get; set; }
+        protected override void OnInitialized()
+        {
+            p_arrDia_Chi = _db_Dia_Chi.List_AD_Dia_Chi();
+            p_objItem = p_arrDia_Chi.FirstOrDefault();
+            
+        }
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await IJS.InvokeVoidAsync("initMap",null);
+                await IJS.InvokeVoidAsync("initAutocompleteAddress_User");
+                StateHasChanged();
+            }
+        }
+        async void Submit(MouseEventArgs args)
+        {
+            await IJS.InvokeVoidAsync("Marker_Address_User", p_strAddress_User);
+        }
+    }
+}
---
+++ Web_App_Medical/wwwroot/Long_Customize/Map.js
@@ -0,0 +1,32 @@
+﻿var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 10.776530, lng: 106.700980 },
+        zoom: 10,
+    });
+}
+function initAutocompleteAddress_User() {
+    var input = document.getElementById('Search_Address_User');
+    const options = {
+        componentRestrictions: { country: "VN" },
+        fields: ["formatted_address", "geometry", "name"],
+        origin: map.getCenter(),
+        strictBounds: false,
+        types: ["establishment"],
+    };
+    var autocomplete = new google.maps.places.Autocomplete(input,options);
+}
+function Marker_Address_User(p_strAddress_User) {
+    var address = p_strAddress_User;
+    console.log(address);
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': address}, function (results, status) {
+        if (status === 'OK') {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position:results[0].geometry.location
+            });
+        }
+    });
+}
